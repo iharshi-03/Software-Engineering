d08067015224a1ae71f2a99066fe0832638ffc94
iharshi-03 <harshininiruphama@gmail.com>
2026-05-03 18:15:25 +0530

    Add Experiment 15 buggy code and project management doc

---
+++ 15/team_calc.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    # Intentional bug: missing parenthesis in print statement
+    print("The sum is: " + str(a + b)
+    return a + b
+
+if __name__ == '__main__':
+    add_numbers(5, 10)
