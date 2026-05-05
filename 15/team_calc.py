53b09538d6b28dc6d229797011837243546af05a
iharshi-03 <harshininiruphama@gmail.com>
2026-05-05 12:58:50 +0530

    Fixed syntax error in team script

--- 15/team_calc.py
+++
@@ -1,7 +0,0 @@
-def add_numbers(a, b):
-    # Intentional bug: missing parenthesis in print statement - FIXED
-    print("The sum is: " + str(a + b))
-    return a + b
-
-if __name__ == '__main__':
-    add_numbers(5, 10)
