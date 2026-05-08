68c99d7a95e32b92085147bc0846cdbabd5207cb
iharshi-03 <harshininiruphama@gmail.com>
2026-05-08 09:33:06 +0530

    Emergency upload of all lab screenshots

--- 16/app.py
+++ 16/app.py
@@ -4,4 +4,4 @@ todos = ["Finish SE Lab", "Learn Docker", "Push to GitHub"]
 HTML = '<html><body><h2>My To-Do List</h2><ul>{% for i in items %}<li>{{i}}</li>{% endfor %}</ul></body></html>'
 @app.route("/")
 def index(): return render_template_string(HTML, items=todos)
-if __name__ == "__main__": app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__": app.run(host="127.0.0.1", port=5000)
